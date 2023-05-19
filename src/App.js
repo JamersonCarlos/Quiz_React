5ec840aebb0aea8cc292fda2835df8c675dc0a58
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-19 02:37:24 -0300

    map routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import CreateQuestion from './pages/CreateQuestion';
+import Quiz from './pages/Quiz';
+import Questions from './pages/Questions';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <h1 className='title'>Quiz de Programação</h1>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/quiz' element={<Quiz/>}></Route>
+          <Route path='/quiz/:category' element={<Questions/>}></Route>
+          <Route path='/create' element={<CreateQuestion/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
